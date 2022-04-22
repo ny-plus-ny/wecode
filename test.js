693ee30710d034719827ef419cea0e6b9b063183
ny-plus-ny <ny.plus.ny@gmail.com>
2022-04-22 11:13:47 +0900

    test complete

---
+++ test.js
@@ -0,0 +1,13 @@
+
+function newArr() {
+   
+    const bucket = [];
+      
+    for(let i = 1; i <51; i++) {
+      if (i % 2 === 0 ){
+          bucket.push(i);
+         } 
+      } 
+      return bucket;
+    }
+    
